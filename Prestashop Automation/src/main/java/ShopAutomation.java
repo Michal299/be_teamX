cc7e57293a076b61baab9fce867b46c1b5c4218f
Aleksandra Nadzieja <ola.nadzieja@wp.pl>
2021-11-17 21:22:00 +0100

    Changes in Prestashop Automation

--- Prestashop Automation/src/main/java/ShopAutomation.java
+++ Prestashop Automation/src/main/java/ShopAutomation.java
@@ -1,4 +1,5 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
@@ -179,6 +180,147 @@ public class ShopAutomation {
 
         // Save button
         driver.findElement(By.cssSelector("#form-preferences-save-button")).click();
+    }
+
+    @Test
+    public void buyProducts() throws InterruptedException {
+        // Navigate to the URL
+        driver.get("http://localhost:8001");
+
+        //Maximize current window
+        driver.manage().window().maximize();
+
+        // Click "Repertuar"
+        driver.findElement(By.xpath("//*[@id=\"category-7\"]/a")).click();
+
+        // Click "Bełchatów"
+        driver.findElement(By.xpath("//*[@id=\"left-column\"]/div[1]/ul/li[2]/ul/li[1]/a")).click();
+
+        // Click the element to buy
+        driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/div[1]/div/article/div/div[1]/h2/a")).click();
+
+        // Click to select more elements
+        for(int i = 0 ; i < 3; i++)
+            driver.findElement(By.xpath("//*[@id=\"add-to-cart-or-refresh\"]/div[2]/div/div[1]/div/span[3]/button[1]")).click();
+
+        // Click the button to add the element to cart
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-or-refresh\"]/div[2]/div/div[2]/button")).click();
+
+        // Click the button to continue shopping
+        driver.findElement(By.xpath("//*[@id=\"blockcart-modal\"]/div/div/div[2]/div/div[2]/div/div/button")).click();
+
+        // Click the button to continue shopping
+        driver.findElement(By.xpath("//*[@id=\"blockcart-modal\"]/div/div/div[2]/div/div[2]/div/div/button")).click();
+
+        // Click "Repertuar"
+        driver.findElement(By.xpath("//*[@id=\"category-7\"]/a")).click();
+
+        // Click "Białystok"
+        driver.findElement(By.xpath("//*[@id=\"left-column\"]/div[1]/ul/li[2]/ul/li[2]/a")).click();
+
+        // Click the element to buy
+        driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/div[1]/div[1]/article/div/div[1]/h2/a")).click();
+
+        // Click to select more elements
+        for(int i = 0 ; i < 7; i++)
+            driver.findElement(By.xpath("//*[@id=\"add-to-cart-or-refresh\"]/div[2]/div/div[1]/div/span[3]/button[1]")).click();
+
+        // Click the button to add the element to cart
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-or-refresh\"]/div[2]/div/div[2]/button")).click();
+
+        // Click to process the order
+        driver.findElement(By.xpath("//*[@id=\"blockcart-modal\"]/div/div/div[2]/div/div[2]/div/div/a")).click();
+
+        // Click to remove the item from the order
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[1]/div/div[2]/ul/li[1]/div/div[3]/div/div[3]")).click();
+
+        // Click to move to finish processing the order
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[2]/div/div[2]/div/a")).click();
+
+        // Click to select the gender of the user
+        driver.findElement(By.id("field-id_gender-1")).click();
+
+        // Fill in the first name
+        WebElement first_name = driver.findElement(By.id("field-firstname"));
+        first_name.clear();
+        first_name.sendKeys("User");
+
+        // Fill in the last name
+        WebElement last_name = driver.findElement(By.id("field-lastname"));
+        last_name.clear();
+        last_name.sendKeys("User");
+
+        // Fill in the email
+        WebElement email = driver.findElement(By.id("field-email"));
+        email.clear();
+        email.sendKeys("user@mail.com");
+
+        // Fill in the password
+        WebElement password = driver.findElement(By.id("field-password"));
+        password.clear();
+        password.sendKeys("password123");
+
+        // Fill in the birthday
+        WebElement birthday = driver.findElement(By.id("birthday"));
+        birthday.clear();
+        birthday.sendKeys("2002-07-19");
+
+        // Click to select the first permission
+        driver.findElement(By.xpath("//*[@id=\"customer-form\"]/div/div[8]/div[1]/span/label")).click();
+
+        // Click to select the second permission
+        driver.findElement(By.xpath("//*[@id=\"customer-form\"]/div/div[9]/div[1]/span/label")).click();
+
+        // Click to proceed
+        driver.findElement(By.xpath("//*[@id=\"customer-form\"]/footer/button")).click();
+
+        // Fill in the address
+        WebElement address1 = driver.findElement(By.id("field-address1"));
+        address1.clear();
+        address1.sendKeys("Good Street");
+
+        // Fill in the post code
+        WebElement postcode = driver.findElement(By.id("field-postcode"));
+        postcode.clear();
+        postcode.sendKeys("80-144");
+
+        // Fill in the city
+        WebElement city = driver.findElement(By.id("field-city"));
+        city.clear();
+        city.sendKeys("Warszawa");
+
+        // Fill in the phone
+        WebElement phone = driver.findElement(By.id("field-phone"));
+        phone.clear();
+        phone.sendKeys("500224517");
+
+        // Click to proceed
+        driver.findElement(By.xpath("//*[@id=\"delivery-address\"]/div/footer/button")).click();
+
+        // Click to select the delivery type
+        driver.findElement(By.xpath("//*[@id=\"delivery_option_5\"]")).click();
+
+        // Click to proceed
+        driver.findElement(By.xpath("//*[@id=\"js-delivery\"]/button")).click();
+
+        // Click to select the type of payment
+        driver.findElement(By.xpath("//*[@id=\"payment-option-2\"]")).click();
+
+        // Click to agree to terms and conditions
+        driver.findElement(By.xpath("//*[@id=\"conditions_to_approve[terms-and-conditions]\"]")).click();
+
+        // Click to place the order
+        driver.findElement(By.xpath("//*[@id=\"payment-confirmation\"]/div[1]/button")).click();
+
+        // Click to go to the user's account
+        driver.findElement(By.xpath("//*[@id=\"_desktop_user_info\"]/div/a[2]/span")).click();
+
+        // Click to go to the history of orders
+        driver.findElement(By.xpath("//*[@id=\"history-link\"]/span")).click();
+
+        // Assert that the order was placed
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[4]/span")).isDisplayed());
+
     }
 
 }
