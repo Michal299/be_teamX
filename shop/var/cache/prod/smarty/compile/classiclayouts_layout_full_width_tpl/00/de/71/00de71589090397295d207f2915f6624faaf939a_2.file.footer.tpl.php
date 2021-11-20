1875c31cf2cb8956efb5d2147d53e7c48b46d47f
Winetq <winetq9679@gmail.com>
2021-11-20 10:52:40 +0100

    SSL and SMTP provided

---
+++ shop/var/cache/prod/smarty/compile/classiclayouts_layout_full_width_tpl/00/de/71/00de71589090397295d207f2915f6624faaf939a_2.file.footer.tpl.php
@@ -0,0 +1,136 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-11-20 10:43:36
+  from '/var/www/html/themes/classic/templates/_partials/footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6198c348b2ad77_13213689',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '00de71589090397295d207f2915f6624faaf939a' => 
+    array (
+      0 => '/var/www/html/themes/classic/templates/_partials/footer.tpl',
+      1 => 1637275747,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6198c348b2ad77_13213689 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<div class="container">
+  <div class="row">
+    <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_19111401556198c348b04fc7_32864367', 'hook_footer_before');
+?>
+
+  </div>
+</div>
+<div class="footer-container">
+  <div class="container">
+    <div class="row">
+      <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_48569256198c348b05f12_22046454', 'hook_footer');
+?>
+
+    </div>
+    <div class="row">
+      <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_17900894276198c348b06b13_87155329', 'hook_footer_after');
+?>
+
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <p class="text-sm-center">
+          <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_3214881946198c348b079f9_66307011', 'copyright_link');
+?>
+
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<?php }
+/* {block 'hook_footer_before'} */
+class Block_19111401556198c348b04fc7_32864367 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'hook_footer_before' => 
+  array (
+    0 => 'Block_19111401556198c348b04fc7_32864367',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+      <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['hook'][0], array( array('h'=>'displayFooterBefore'),$_smarty_tpl ) );?>
+
+    <?php
+}
+}
+/* {/block 'hook_footer_before'} */
+/* {block 'hook_footer'} */
+class Block_48569256198c348b05f12_22046454 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'hook_footer' => 
+  array (
+    0 => 'Block_48569256198c348b05f12_22046454',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+        <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['hook'][0], array( array('h'=>'displayFooter'),$_smarty_tpl ) );?>
+
+      <?php
+}
+}
+/* {/block 'hook_footer'} */
+/* {block 'hook_footer_after'} */
+class Block_17900894276198c348b06b13_87155329 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'hook_footer_after' => 
+  array (
+    0 => 'Block_17900894276198c348b06b13_87155329',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+        <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['hook'][0], array( array('h'=>'displayFooterAfter'),$_smarty_tpl ) );?>
+
+      <?php
+}
+}
+/* {/block 'hook_footer_after'} */
+/* {block 'copyright_link'} */
+class Block_3214881946198c348b079f9_66307011 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'copyright_link' => 
+  array (
+    0 => 'Block_3214881946198c348b079f9_66307011',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+            <a href="https://www.prestashop.com" target="_blank" rel="noopener noreferrer nofollow">
+              <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['l'][0], array( array('s'=>'%copyright% %year% - Ecommerce software by %prestashop%','sprintf'=>array('%prestashop%'=>'PrestaShop™','%year%'=>date('Y'),'%copyright%'=>'©'),'d'=>'Shop.Theme.Global'),$_smarty_tpl ) );?>
+
+            </a>
+          <?php
+}
+}
+/* {/block 'copyright_link'} */
+}
