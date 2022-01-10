e4db03ec62031b5ea5f293cbaa734d5dfdcbe503
Natalia Mierkiewicz <57463209+MierkiewiczN@users.noreply.github.com>
2022-01-10 21:06:24 +0100

    fixed url

--- mysql/init/init.sh
+++ mysql/init/init.sh
@@ -3,4 +3,4 @@ mysql -pstudent -e "CREATE USER IF NOT EXISTS BE_175132_user IDENTIFIED BY 'pass
 mysql -pstudent -e "GRANT ALL PRIVILEGES ON BE_175132.* TO 'BE_175132_user';"
 mysql -pstudent -e "FLUSH PRIVILEGES;"
 mysql -u BE_175132_user -ppassword BE_175132 < db_dump.sql		# odzyskiwanie dumpa bazy danych
-mysql -u BE_175132_user -ppassword BE_175132 -e "UPDATE ps_shop_url SET domain='localhost:15132', domain_ssl='localhost:1533' WHERE id_shop_url=1;"	# ustawienie url i ssl url dla sklepu
+mysql -u BE_175132_user -ppassword BE_175132 -e "UPDATE ps_shop_url SET domain='localhost:15132', domain_ssl='localhost:15133' WHERE id_shop_url=1;"	# ustawienie url i ssl url dla sklepu
